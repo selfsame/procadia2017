ce92bec17096604ed0fe01086cda8611380ab064
Joseph Parker <joseph.parker.1@gmail.com>
2017-11-05 11:40:06 -0500

    game.entity part & make-entity

--- Assets/game/entity.clj
+++ Assets/game/entity.clj
@@ -2,7 +2,70 @@
   (use
     arcadia.core
     arcadia.linear
-    hard.core))
+    hard.core
+    hard.seed))
+
+(def PARTS (atom {}))
+
+(defn part [{:keys [prefab type mount-points hooks state id] :as part-map}]
+  (let [id (or id (hash (dissoc part-map :hooks)))
+        part-map (assoc part-map :id id)]
+    (swap! PARTS update-in [type] assoc id part-map)))
+
+(defn parts-typed [k]
+  (-> @PARTS k vals))
+
+(defn probability [col]
+  (let [total (reduce + (vals col))
+        roll (srand total)]
+    (loop [xs col
+           acc 0]
+      (if (< roll (+ acc (last (first xs))))
+          (ffirst xs)
+          (recur (rest xs) (+ acc (last (first xs))))))))
+
+
+(defn attach [mount m]
+  (when-let [obj (clone! (:prefab m))]
+    (parent! obj mount)
+    (position! obj (>v3 mount))
+    (rotation! obj (.rotation (.transform mount)))
+    (dorun
+      (map 
+        (fn [[k v]]
+          (when-let [mount (child-named obj (name k))]
+            (when-let [m (srand-nth (vec (parts-typed (probability v))))]
+              (attach mount m))))
+        (:mount-points m)))))
 
 (defn make-entity [budget]
-  (clone! :player))
+  (let [root (clone! :entity)
+        parts (atom [])]
+    (when-let [start (srand-nth (vec (parts-typed :body)))]
+      (attach root start))
+    root))
+
+
+(part {
+  :type :body
+  :id :business
+  :prefab :parts/business-body
+  :mount-points {
+    :neck {:head 1}
+    :left-arm {:arm 1 :head 1}
+    :right-arm {:arm 1 :head 1}
+  } 
+  :hooks {:aim (fn [root this aim])}})
+
+(part {
+  :type :head
+  :id :business
+  :prefab :parts/business-head})
+
+(part {
+  :type :arm
+  :id :business
+  :prefab :parts/business-arm})
+
+'(do (clear-cloned!)
+  (make-entity 0))
