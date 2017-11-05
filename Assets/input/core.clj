d61a4d847760f88c3fbccf56a5c16e5cde1e725f
Joshua Suskalo <JZSuskalo@FullSail.edu>
2017-11-04 22:21:22 -0400

    Update input to use a plane at the player's position

--- Assets/input/core.clj
+++ Assets/input/core.clj
@@ -1,7 +1,7 @@
 (ns input.core
   (:require [arcadia.core :as arc]
-           [arcadia.linear :as lin])
-  (:import [UnityEngine Input Vector2 Camera]))
+            [arcadia.linear :as lin])
+  (:import [UnityEngine Input Vector3 Camera Physics Plane Ray]))
 
 (def input-axes {:horizontal "Horizontal"
                  :vertical "Vertical"})
@@ -20,14 +20,22 @@
                   (seq bttns))))))
 
 (defn update-input-state!
-  [old-state player-pos]
+  [old-state ^Vector3 player-pos layer]
   {:movement (.normalized (lin/v2 (Input/GetAxisRaw (input-axes :horizontal))
                               (Input/GetAxisRaw (input-axes :vertical))))
    ;; TODO: update aim to use a raycast from the camera through the mouse rather
    ;; than a direct mouse position, so that aiming doesn't care about camera angle
-   :aim (.normalized (lin/v2- (let [pos (.ScreenToWorldPoint Camera/main Input/mousePosition)]
-                        (lin/v2 (.x pos) (.y pos)))
-                      (lin/v2 (.x player-pos) (.y player-pos))))
+   :aim (let [plane (Plane. Vector3/up player-pos)
+              ray (.ScreenPointToRay Camera/main Input/mousePosition)
+              dist (float 0.0)
+              max-dist (float 100.0)]
+          (if (.Raycast plane ray (by-ref dist))
+            (.GetPoint ray dist)
+            (.GetPoint ray max-dist)))
+
+   #_(.normalized (lin/v2- (let [pos (.ScreenToWorldPoint Camera/main Input/mousePosition)]
+                                (lin/v2 (.x pos) (.y pos)))
+                              (lin/v2 (.x player-pos) (.y player-pos))))
    :buttons-down (get-buttons #(Input/GetMouseButtonDown %) input-buttons)
    :buttons-pressed (get-buttons #(Input/GetMouseButton %) input-buttons)
    :buttons-up (get-buttons #(Input/GetMouseButtonUp %) input-buttons)})
@@ -39,4 +47,6 @@
   [this key]
   (let [other (arc/state this :output-obj)]
     (when other
-      (arc/update-state other :input update-input-state! (.position (.transform this))))))
+      (arc/update-state other :input update-input-state!
+                        (.position (.transform other))
+                        (arc/state this :raycast-layer)))))
