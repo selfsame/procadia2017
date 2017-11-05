ca64d576fdc432dc6b25d9e9a1d26df2d1ee0b0a
Joshua Suskalo <JZSuskalo@FullSail.edu>
2017-11-04 22:39:34 -0400

    Pull Plane Raycast out to function to help with magic compilation

--- Assets/input/core.clj
+++ Assets/input/core.clj
@@ -1,6 +1,7 @@
 (ns input.core
   (:require [arcadia.core :as arc]
-            [arcadia.linear :as lin])
+            [arcadia.linear :as lin]
+            [magic.api :as m])
   (:import [UnityEngine Input Vector3 Camera Physics Plane Ray]))
 
 (def input-axes {:horizontal "Horizontal"
@@ -19,19 +20,23 @@
              (map (fn [[key val]] [key (fun val)])
                   (seq bttns))))))
 
+(m/defn get-mouse-on-plane!
+  [^Vector3 player-pos]
+  (let [plane (Plane. Vector3/up player-pos)
+        ray (.ScreenPointToRay Camera/main Input/mousePosition)
+        dist (float 0)
+        max-dist (float 100)]
+    (if (.Raycast plane ray (by-ref dist))
+      (.GetPoint ray dist)
+      (.GetPoint ray max-dist))))
+
 (defn update-input-state!
-  [old-state ^Vector3 player-pos layer]
+  [old-state player-pos]
   {:movement (.normalized (lin/v2 (Input/GetAxisRaw (input-axes :horizontal))
                               (Input/GetAxisRaw (input-axes :vertical))))
    ;; TODO: update aim to use a raycast from the camera through the mouse rather
    ;; than a direct mouse position, so that aiming doesn't care about camera angle
-   :aim (let [plane (Plane. Vector3/up player-pos)
-              ray (.ScreenPointToRay Camera/main Input/mousePosition)
-              dist (float 0.0)
-              max-dist (float 100.0)]
-          (if (.Raycast plane ray (by-ref dist))
-            (.GetPoint ray dist)
-            (.GetPoint ray max-dist)))
+   :aim (get-mouse-on-plane! player-pos)
 
    #_(.normalized (lin/v2- (let [pos (.ScreenToWorldPoint Camera/main Input/mousePosition)]
                                 (lin/v2 (.x pos) (.y pos)))
@@ -48,5 +53,4 @@
   (let [other (arc/state this :output-obj)]
     (when other
       (arc/update-state other :input update-input-state!
-                        (.position (.transform other))
-                        (arc/state this :raycast-layer)))))
+                        (.position (.transform other))))))
